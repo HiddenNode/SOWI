a27479271ca2d97218a644e951e848f0673b574c
HiddenNode <hiddingnode@gmail.com>
2022-04-14 17:56:52 +0000

    switched to using CMake instead of make

---
+++ binary/CMakeFiles/sowa.exe.dir/cmake_clean.cmake
@@ -0,0 +1,35 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sowa.exe.dir/Light.cpp.obj"
+  "CMakeFiles/sowa.exe.dir/Light.cpp.obj.d"
+  "CMakeFiles/sowa.exe.dir/camera.cpp.obj"
+  "CMakeFiles/sowa.exe.dir/camera.cpp.obj.d"
+  "CMakeFiles/sowa.exe.dir/commandHandler.cpp.obj"
+  "CMakeFiles/sowa.exe.dir/commandHandler.cpp.obj.d"
+  "CMakeFiles/sowa.exe.dir/commands.cpp.obj"
+  "CMakeFiles/sowa.exe.dir/commands.cpp.obj.d"
+  "CMakeFiles/sowa.exe.dir/eventHandler.cpp.obj"
+  "CMakeFiles/sowa.exe.dir/eventHandler.cpp.obj.d"
+  "CMakeFiles/sowa.exe.dir/helper.cpp.obj"
+  "CMakeFiles/sowa.exe.dir/helper.cpp.obj.d"
+  "CMakeFiles/sowa.exe.dir/main.cpp.obj"
+  "CMakeFiles/sowa.exe.dir/main.cpp.obj.d"
+  "CMakeFiles/sowa.exe.dir/program.cpp.obj"
+  "CMakeFiles/sowa.exe.dir/program.cpp.obj.d"
+  "CMakeFiles/sowa.exe.dir/resourceHandler.cpp.obj"
+  "CMakeFiles/sowa.exe.dir/resourceHandler.cpp.obj.d"
+  "CMakeFiles/sowa.exe.dir/shader.cpp.obj"
+  "CMakeFiles/sowa.exe.dir/shader.cpp.obj.d"
+  "CMakeFiles/sowa.exe.dir/stb_image.cpp.obj"
+  "CMakeFiles/sowa.exe.dir/stb_image.cpp.obj.d"
+  "CMakeFiles/sowa.exe.dir/texture.cpp.obj"
+  "CMakeFiles/sowa.exe.dir/texture.cpp.obj.d"
+  "libsowa.exe.dll.a"
+  "sowa.exe.exe"
+  "sowa.exe.exe.manifest"
+  "sowa.exe.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/sowa.exe.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
